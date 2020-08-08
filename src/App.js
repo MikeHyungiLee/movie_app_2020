1a69f2d3f94ff066182a02f9fc9a8216be12fb62
Hyungi's portfolio <mike.hyungi.lee@gmail.com>
2020-08-09 01:20:52 +0900

    [update] practice state part in reactJS.

--- src/App.js
+++ src/App.js
@@ -1,51 +1,29 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function Food({ name, picture }) {
-    return ( <
-        div >
-        <
-        h2 > I like { name } < /h2> <img src={picture} width="300" heith="300" / >
-        <
-        /div>
-    );
-}
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("hello");
+  }
+  // 데이터는 변한다. 화면에 보이는 데이터 중에서 바꾸고 싶은 데이터를 state에 넣어준다.
+  state = {
+    isLoading: true,
+    movies: [],
+  };
+  componentDidMount() {
+    // fetch data from API
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 6000);
+  }
+  render() {
+    // ES6 JS - ES6 JS코스 강의 보기
+    const { isLoading } = this.state;
+    // fetch the data from API in componentDidMount method, then map the movie list in here!
 
-// data array
-const foodILike = [{
-        name: "Kimchi",
-        image: "https://www.koreanbapsang.com/wp-content/uploads/2016/10/DSC_1843-e1477360668451.jpg",
-        rating: 4.5,
-    },
-    {
-        name: "Samgyeopsam",
-        image: "https://i.pinimg.com/originals/bb/be/b3/bbbeb3054f2d5ab7312624a15ee3614e.jpg",
-        rating: 5.0,
-    },
-    {
-        name: "NangMyeon",
-        image: "https://spainvskorearecipes.files.wordpress.com/2017/09/1732e8aa-b9e6-4b90-b4f2-ee2a057b170f-1.jpg",
-        rating: 4.1,
-    },
-    {
-        name: "Kalguksu",
-        image: "https://mykoreankitchen.com/wp-content/uploads/2016/12/1.-Kalguksu-500x500.jpg",
-        rating: 3.8,
-    },
-];
-
-function App() {
-    return ( <
-        div >
-        <
-        h1 > Hello!!!!! < /h1>{" "} {
-            foodILike.map((dish) => ( <
-                Food name = { dish.name }
-                picture = { dish.image }
-                />
-            ))
-        } { " " } <
-        /div>
-    );
+    return <div>{isLoading ? "Loading" : "We are ready"}</div>;
+  }
 }
 
 export default App;
