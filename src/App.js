b2f7147e351539c482a978df1e49b055919ea29e
Hyungi's portfolio <mike.hyungi.lee@gmail.com>
2020-08-09 22:38:06 +0900

    [update] fetch movie information from api server.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,66 @@
 import React from "react";
 import PropTypes from "prop-types";
+import axios from "axios";
+import Movie from "./Movie";
+import "./App.css";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     console.log("hello");
   }
+
   // 데이터는 변한다. 화면에 보이는 데이터 중에서 바꾸고 싶은 데이터를 state에 넣어준다.
   state = {
     isLoading: true,
     movies: [],
   };
+
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+
+    // this.setState({movies(from state):movies(from)})
+    //= this.setState({movies: movies})
+    this.setState({ movies, isLoading: false });
+  };
+
   componentDidMount() {
-    // fetch data from API
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 6000);
+    this.getMovies();
   }
+
   render() {
     // ES6 JS - ES6 JS코스 강의 보기
-    const { isLoading } = this.state;
-    // fetch the data from API in componentDidMount method, then map the movie list in here!
-
-    return <div>{isLoading ? "Loading" : "We are ready"}</div>;
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map((movie) => (
+              <Movie
+                // child list should have a unique "key" prop.
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}
+      </section>
+    );
   }
 }
 
